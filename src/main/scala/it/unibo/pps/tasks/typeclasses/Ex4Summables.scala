c34d4f11ddab63a9e4642193b654b11c46a0d8c1
Samuele Ferri <samuele1301@gmail.com>
2026-03-29 16:20:44 +0200

    feat: Ex4Summables done

--- src/main/scala/it/unibo/pps/tasks/typeclasses/Ex4Summables.scala
+++ src/main/scala/it/unibo/pps/tasks/typeclasses/Ex4Summables.scala
@@ -10,7 +10,7 @@ import it.unibo.pps.u03.Sequences.Sequence, Sequence.*
 
 object Ex4Summables:
 
-  def sumAllInt(seq: Sequence[Int]): Int = seq match
+  private def sumAllInt(seq: Sequence[Int]): Int = seq match
     case Cons(h, t) => h + sumAllInt(t)
     case _ => 0
 
@@ -18,22 +18,29 @@ object Ex4Summables:
     def sum(a1: A, a2: A): A
     def zero: A
 
-  def sumAll[A: Summable](seq: Sequence[A]) =
+  private def sumAll[A: Summable](seq: Sequence[A]) =
     val summable = summon[Summable[A]]
-    ???  // complete here
+    def recSumAll(seq: Sequence[A]): A = seq match
+      case Cons(h, t) => summable.sum(h, recSumAll(t))
+      case _ => summable.zero
+    recSumAll(seq)
 
   given Summable[Int] with
     def sum(a1: Int, a2: Int): Int = a1 + a2
     def zero: Int = 0
-  
-  // write givens for Summable[Double] and Summable[String]
 
-  @main def trySummables =
+  given Summable[Double] with
+    def sum(a1: Double, a2: Double): Double = a1 + a2
+    def zero: Double = 0.0
+
+  given Summable[String] with
+    def sum(a1: String, a2: String): String = a1 + a2
+    def zero: String = ""
+
+  @main def trySummables(): Unit =
     val si = Cons(10, Cons(20, Cons(30, Nil())))  
     println:
       sumAllInt(si) // 60
-
-    /* uncomment from here   
 
     println:
       sumAll(si) // 60
@@ -45,6 +52,4 @@ object Ex4Summables:
     val ss = Cons("10", Cons("20", Cons("30", Nil())))  
     println:
       sumAll(ss) // "102030"
-
-    */  
 
