5a845b4c5859ffc67670e6fd3f3b838610409c4e
Samuele Ferri <samuele1301@gmail.com>
2026-03-29 16:52:10 +0200

    feat: Ex5Traversable done

--- src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,9 @@
 package it.unibo.pps.tasks.typeclasses
 
-import it.unibo.pps.u03.Sequences.Sequence, Sequence.*
+import it.unibo.pps.u03.Sequences.Sequence
+import Sequence.*
+import it.unibo.pps.u03.Optionals.Optional
+import it.unibo.pps.u03.Optionals.Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -17,10 +20,45 @@ import it.unibo.pps.u03.Sequences.Sequence, Sequence.*
 
 object Ex5Traversable:
 
-  def log[A](a: A): Unit = println("The next element is: "+a)
+  private def log[A](a: A): Unit = println("The next element is: " + a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+//  def logAll[A](seq: Sequence[A]): Unit = seq match
+//    case Cons(h, t) => log(h); logAll(t)
+//    case _ => ()
 
+  trait Traversable[T[_]]:
+    def traverse[A](container: T[A])(consumer: A => Unit): Unit
+
+  private def logAll[T[_]: Traversable, A](container: T[A])(consumer: A => Unit): Unit =
+    val traversable = summon[Traversable[T]]
+    traversable.traverse(container)(consumer)
+
+  given Traversable[Sequence] with
+    def traverse[A](container: Sequence[A])(consumer: A => Unit): Unit = container match
+      case Cons(h, t) => consumer(h); traverse(t)(consumer)
+      case _ => ()
+
+  given Traversable[Optional] with
+    def traverse[A](container: Optional[A])(consumer: A => Unit): Unit = container match
+      case Just(value) => consumer(value)
+      case _ => ()
+
+  @main def tryTraversable(): Unit =
+    val seq = Cons(10, Cons(20, Cons(30, Nil())))
+    val opt = Just(42)
+    val emptyOpt: Optional[Int] = Empty()
   
+    println("Sequence con log")
+    logAll(seq)(log)
+  
+    println("\nSequence con println")
+    logAll(seq)(println(_))
+  
+    println("\nOptional con log")
+    logAll(opt)(log)
+  
+    println("\nOptional con println")
+    logAll(opt)(println(_))
+  
+    println("\nEmpty Optional con log")
+    logAll(emptyOpt)(log)
